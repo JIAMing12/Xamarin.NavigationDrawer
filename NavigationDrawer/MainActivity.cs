2be1e5e1ca00133a54cc6f1d436c454258d9c88e
likky <664907696@qq.com>
2018-09-16 14:33:54 +0800

    添加项目文件。

---
+++ NavigationDrawer/MainActivity.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.Widget;
+using Android.Support.V4.View;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Support.Design.Widget;
+
+namespace NavigationDrawer
+{
+    [Activity(Label = "NavigationDrawer Test", MainLauncher = true, Icon = "@drawable/icon",Theme = "@style/AppTheme.NoActionBar")]
+    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
+    {
+        public DrawerLayout drawerLayout;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Create UI
+            SetContentView(Resource.Layout.activity_main);
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            // Init toolbar
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            // Attach item selected handler to navigation view
+            var navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+
+            // Create ActionBarDrawerToggle button and add it to the toolbar
+            var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            drawerLayout.AddDrawerListener(drawerToggle);
+            drawerToggle.SyncState();
+
+            FloatingActionButton fab= FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += delegate
+            {
+                Toast.MakeText(this, "Replace with your own action", ToastLength.Short).Show();
+               
+            };
+
+
+        }
+
+
+        public override void OnBackPressed()
+        {
+            if (drawerLayout.IsDrawerOpen(GravityCompat.Start))
+            {
+                drawerLayout.CloseDrawer(GravityCompat.Start);
+
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            MenuInflater.Inflate(Resource.Menu.main,menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.ItemId;
+            //==才是比较符号
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+
+        public bool OnNavigationItemSelected(IMenuItem menuItem)
+        {
+            switch (menuItem.ItemId)
+            {
+                case (Resource.Id.nav_camera):
+                    // React on 'Home' selection
+                    break;
+                case (Resource.Id.nav_gallery):
+                    // React on 'Messages' selection
+                    break;
+                case (Resource.Id.nav_slideshow):
+                    // React on 'Friends' selection
+                    break;
+                case (Resource.Id.nav_manage):
+                    // React on 'Discussion' selection
+                    break;
+                case (Resource.Id.nav_share):
+                    // React on 'Discussion' selection
+                    break;
+                case (Resource.Id.nav_send):
+                    // React on 'Discussion' selection
+                    break;
+            }
+
+            // Close drawer
+            drawerLayout.CloseDrawers();
+            return true;
+        }
+
+    }
+}
